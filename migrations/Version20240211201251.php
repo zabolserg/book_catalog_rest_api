b8daa5059f38e59c83c7842ea0fe20e0317320d1
Sergii Zabolotnyi <zabolserg@ukr.net>
2024-02-11 22:15:53 +0200

    Author_Book migration

---
+++ migrations/Version20240211201251.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240211201251 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE author_book (
+            author_id INT NOT NULL,
+            book_id INT NOT NULL,
+            PRIMARY KEY (author_id, book_id),
+            FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE CASCADE,
+            FOREIGN KEY (book_id) REFERENCES book(id) ON DELETE CASCADE
+        )');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE author_book');
+    }
+}
